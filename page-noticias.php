6997939a8fba3635abcbe914dcde209ef86cc26b
ErwiseDevelopment <erwise.dev@gmail.com>
2023-02-22 00:18:12 -0300

    ajsutes noticias

--- page-noticias.php
+++ page-noticias.php
@@ -54,50 +54,48 @@ get_header(); ?>
 
                             <!-- loop -->
                             <?php 
-     
+     							$link_pattern = get_field( 'link_padrao_portal', 'option' );
+                                 $post_link = $link_pattern . get_field( 'link_caminho', 'option' ) . get_field( 'link_noticia', 'option' );
+                                 $request_posts = wp_remote_get( $post_link );
+                               
+         
+                                 if(!is_wp_error( $request_posts )) :
+                                     $body = wp_remote_retrieve_body( $request_posts );
+                                     $data = json_decode( $body );
+         
+                                     if(!is_wp_error( $data )) :
+                                         foreach( $data as $rest_post ) :
                             ?>
                                         <div class="col-lg-6 js-posts-new my-3">
 
                                             <div class="card rounded-0">
-                                                <a class="text-decoration-none " href="<?php the_permalink() ?>">
-                                                    <div class="card-img">
-                                                        <!-- <img
-                                                        class="l-posts__thumbnail img-fluid w-100 u-object-fit-cover"
-                                                        src="<php echo get_home_url( null, '/wp-content/uploads/2022/06/posts-image.png' ) ?>"
-                                                        alt="Post image"> -->
-
-                                                        <?php
-                                                            $alt_title = get_the_title();
-
-                                                            the_post_thumbnail( 'post-thumbnail',
-                                                                array(
-                                                                    'class' => 'l-posts__thumbnail img-fluid w-100 u-object-fit-cover',
-                                                                    'alt'   => $alt_title
-                                                                ));
-                                                        ?>
-                                                    </div>
-
+                                                <a class="text-decoration-none " href="<?php echo get_home_url( null, 'noticia/?id=' . $rest_post->id )  ?>">
+                                                <div class="card-img w-100">
+                                                    <img
+                                                    class="img-fluid w-100 u-object-fit-cover"
+                                                    src="<?php echo $data->featured_image_src; ?>"
+                                                        alt="<?php echo $data->title->rendered; ?>">
+                                                </div>
                                                     <div class="card-body px-4">
 
                                                         <p class="u-font-size-12 xxl:u-font-size-14 u-font-weight-semibold u-color-folk-medium-electric-blue mb-0">
-                                                            <!-- 10 de dezembro de 2021 -->
-                                                            <?php echo get_date_format( get_the_date( 'd/m/Y', get_the_ID() ) ) ?>
-                                                        </p>
-
-                                                        <p class="u-font-size-12 xxl:u-font-size-14 u-font-weight-bold u-color-folk-medium-electric-blue">
-                                                            <!-- Institucional -->
-                                                            <?php
-                                                                foreach( $post_categories as $post_category ) 
-                                                                    echo $post_category . ' ';
+                                                            <?php $date_post = $data->date;
+                                                                list($data_year, $data_month, $data_day) = explode("-", $date_post);
+                                                                list($data_day1) = explode("T", $data_day);
+                                                                echo $data_day1 . '/' . $data_month . '/' . $data_year; 
                                                             ?>
                                                         </p>
 
+                                                        <p class="u-font-size-12 xxl:u-font-size-14 u-font-weight-bold u-color-folk-medium-electric-blue">
+                                                        
+                                                        </p>
+
                                                         <h4 class="u-font-size-16 xxl:u-font-size-20 u-font-weight-bold u-color-folk-bold-electric-blue mb-4">
-                                                            <?php the_title() ?>
+                                                            <?php echo $data->title->rendered; ?>
                                                         </h4>
 
                                                         <span class="d-block u-font-size-14 xxl:u-font-size-16 u-font-weight-regular u-color-folk-aluminium">
-                                                            <?php the_excerpt(); ?>
+                                                            <?php echo $data->content->rendered;  ?>
                                                         </span>
 
                                                         <div class="row">
@@ -114,29 +112,13 @@ get_header(); ?>
 											    </a>
                                             </div>
                                         </div>
+                                        <?php endforeach;
+                                                        endif;
+                                                            endif;?>
 
                             <!-- end loop -->
                         </div>
                     </div>
-
-                    <div class="col-md-4 col-lg-3 order-1 order-md-2 mt-3">
-
-                        <div>
-                            <form class="js-search-form" method="GET" action="/">
-
-                                <div class="row">
-
-                                    <div class="col-12">
-                                        <input
-                                        class="w-100 border-0 d-block u-font-size-14 u-font-weight-semibold u-color-folk-bold-eletric-blue u-bg-folk-light-gray py-4 pl-3 pr-5"
-                                        type="search"
-                                        name="s"
-                                        placeholder="Procurar">
-                                        <span class="l-page-news__icon js-search-submit"></span>
-                                    </div>
-                                </div>
-                            </form>                            
-                        </div>
                         <div class="row">
 
                             <div class="col-12 mt-5">
@@ -154,12 +136,10 @@ get_header(); ?>
         </div>
     </div>
 </section>
-
-<?php endwhile; ?>
-
 </div><!-- #main -->
 </section><!-- #primary -->
 
+<?php endwhile; ?>
 <?php
 
 get_footer();
